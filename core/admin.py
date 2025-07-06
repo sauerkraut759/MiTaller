d7837c4f37975b5c0ece72b9d727983ab75cdb02
Khaseis <sebaguilera153@gmail.com>
2025-07-05 21:54:29 -0400

    registrar modelos en django admin

--- core/admin.py
+++ core/admin.py
@@ -2,3 +2,9 @@ from django.contrib import admin
 from .models import Profesor, Categoria, Lugar, Taller
 
 # Register your models here.
+
+
+admin.site.register(Profesor)
+admin.site.register(Categoria)
+admin.site.register(Lugar)
+admin.site.register(Taller)
