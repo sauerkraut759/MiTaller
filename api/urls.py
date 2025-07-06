c97da1b63b12c82fca02874f25d36fe1fa24c9c8
Khaseis <sebaguilera153@gmail.com>
2025-07-06 01:57:34 -0400

    Permitir que las rutas reciban el parametro id

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 urlpatterns = [
     path('categorias/', views.getCategorias),
+    path('categorias/<int:id>/', views.getCategorias),
     path('lugares/', views.getLugares),
+    path('lugares/<int:id>', views.getLugares),
     path('profesores/', views.getProfesores),
+    path('profesores/<int:id>', views.getProfesores),
     path('talleres/', views.viewTalleres)
 ]
--- api/views.py
+++ api/views.py
@@ -1,41 +1,65 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from django.utils import timezone
 from core.models import Categoria, Lugar, Profesor, Taller
 from .serializers import CategoriaSerializer, ProfesorSerializer, LugarSerializer, TallerSerializer
 
 # Create your views here.
 
 @api_view(['GET'])
-def getCategorias(request):
-    categorias = Categoria.objects.all()
-    serializer = CategoriaSerializer(categorias, many=True)
+def getCategorias(request, id=0):
+
+    if id != 0:
+        categorias = Categoria.objects.get(id=id)
+        serializer = CategoriaSerializer(categorias, many=False)
+    else:
+        categorias = Categoria.objects.all()
+        serializer = CategoriaSerializer(categorias, many=True)
+
     
     return Response(serializer.data)
 
 @api_view(['GET'])
-def getProfesores(request):
-    profesores = Profesor.objects.all()
-    serializer = ProfesorSerializer(profesores, many=True)
+def getProfesores(request, id=0):
+
+    if id != 0:
+        profesores = Profesor.objects.get(id=id)
+        serializer = ProfesorSerializer(profesores, many=False)
+    else:
+        profesores = Profesor.objects.all()
+        serializer = ProfesorSerializer(profesores, many=True)
     
     return Response(serializer.data)
 
 @api_view(['GET'])
 def getLugares(request):
-    lugares = Lugar.objects.all()
-    serializer = LugarSerializer(lugares, many=True)
+    
+    if id != 0:
+        lugares = Lugar.objects.get(id=id)
+        serializer = LugarSerializer(lugares, many=False)
+    else:
+        lugares = Lugar.objects.all()
+        serializer = LugarSerializer(lugares, many=True)
     
     return Response(serializer.data)
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def viewTalleres(request):
 
-    estado = request.query_params.get('estado')
-    if estado:
-        talleres = Taller.objects.filter(estado=estado)
-    else:
-        talleres = Taller.objects.all()
+    if request.method == 'GET':
 
-    serializer = TallerSerializer(talleres, many=True)
+        estado = request.query_params.get('estado')
+        futuros = request.query_params.get('futuros')
+
+        # Si es anonimo o pide solo los futuros devuelve lo correspondiente y si pide un estado especifico lo filtra (estando autenticado)
+        if request.user.is_anonymous or futuros:
+            talleres = Taller.objects.filter(estado='aceptado', fecha__gte=timezone.now().date())
+        elif futuros:
+            talleres = Taller.objects.filter(estado=estado)
+        else:
+            talleres = Taller.objects.all()
+
     
+    serializer = TallerSerializer(talleres, many=True)
     
     return Response(serializer.data)
