140df26822068395d789b5a7532ecda6830a1186
Khaseis <sebaguilera153@gmail.com>
2025-07-07 01:38:37 -0400

    agrega las rutas faltantes

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.loginUser, name='login'),
     path('logout/', views.logoutUser, name='logout'),
-    path('talleres/todos', views.verTalleres, name='verTalleres')
+    path('talleres/todos', views.verTalleres, name='verTalleres'),
+    path('talleres/proponer', views.proponerTaller, name='proponerTaller')
 ]
