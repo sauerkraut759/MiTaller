7a6a8a07330d8b863b9c1755dd076c870864f681
Khaseis <sebaguilera153@gmail.com>
2025-07-07 01:39:25 -0400

    configura para que la api solo permita acceso autenticado

--- mitaller_villaverde/settings.py
+++ mitaller_villaverde/settings.py
@@ -126,10 +126,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
-        # 'rest_framework.authentication.BasicAuthentication',  # opcional
+        'rest_framework.authentication.SessionAuthentication'
     ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',  # o IsAuthenticated si prefieres restringir por defecto
+        'rest_framework.permissions.IsAuthenticated'
     ]
 }
