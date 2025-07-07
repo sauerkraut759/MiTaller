73f8f207c915fa45eadc984b81d6f2ea0a35de8e
Khaseis <sebaguilera153@gmail.com>
2025-07-06 22:37:50 -0400

    anade la clase talleradmin

--- core/admin.py
+++ core/admin.py
@@ -3,8 +3,51 @@ from .models import Profesor, Categoria, Lugar, Taller
 
 # Register your models here.
 
+class TallerAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'titulo',
+        'fecha',
+        'duracion_horas',
+        'profesor',
+        'lugar',
+        'categoria',
+        'estado',
+        'observacion'
+    )
+
+    list_editable = (
+        'profesor',
+        'estado',
+        'observacion'
+    )
+
+    actions = ['aceptar', 'rechazar', 'revisar']
+
+    def aceptar(self, request, queryset):
+        for taller in queryset:
+            taller.estado = 'aceptado'
+            taller.save()
+
+        self.message_user(request, f"{queryset.count()} nuevos talleres aceptados.")
+
+    def rechazar(self, request, queryset):
+        for taller in queryset:
+            taller.estado = 'rechazado'
+            taller.save()
+
+        self.message_user(request, f"{queryset.count()} nuevos talleres rechazados.")
+
+    def revisar(self, request, queryset):
+        for taller in queryset:
+            taller.estado = 'pendiente'
+            taller.save()
+
+        self.message_user(request, f"{queryset.count()} nuevos talleres marcados para revision.")
+        
+
 
 admin.site.register(Profesor)
 admin.site.register(Categoria)
 admin.site.register(Lugar)
-admin.site.register(Taller)
+admin.site.register(Taller, TallerAdmin)
