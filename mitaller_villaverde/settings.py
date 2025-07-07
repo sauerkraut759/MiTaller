42626c5c59ee8208eae7eaa312937e76a2ad7eae
Khaseis <sebaguilera153@gmail.com>
2025-07-06 23:07:37 -0400

    anade configuracion para djangorest

--- mitaller_villaverde/settings.py
+++ mitaller_villaverde/settings.py
@@ -123,3 +123,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        # 'rest_framework.authentication.BasicAuthentication',  # opcional
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.AllowAny',  # o IsAuthenticated si prefieres restringir por defecto
+    ]
+}
