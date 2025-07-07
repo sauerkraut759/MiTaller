3a4150ed1148706aa0c283459383b78fd9a8d7da
Khaseis <sebaguilera153@gmail.com>
2025-07-06 20:57:06 -0400

    Vista home

--- core/urls.py
+++ core/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('login/', views.loginUser, name='login'),
-    path('logout/', views.logoutUser, name='logout')
+    path('logout/', views.logoutUser, name='logout'),
+    path('talleres/todos', views.verTalleres, name='verTalleres')
 ]
--- core/views.py
+++ core/views.py
@@ -1,14 +1,39 @@
+import requests
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from dateutil.parser import parse
 
+from .models import Categoria, Profesor, Lugar
 
-# Create your views here.
+# Create your views here.}
+
+API_URL = 'http://localhost:8000/api/'
 
 def home(request):
 
-    return render(request, 'core/index.html')
+    try:
+        # No estaba seguro si consumir la propia API del sistema era un requerimiento asi que lo hice de todas formas para asegurar
+        # Normalmente obtendria la informacion a traves de los modelos
+        response = requests.get(API_URL + 'talleres?futuros=true')
+        response.raise_for_status()
+        talleres = response.json()
+    except requests.exceptions.RequestException as e:
+        talleres = []
+        print("Error al consultar la API:", e)
+
+    for taller in talleres:
+        taller["fecha"] = parse(taller["fecha"])
+        taller["profesor"] = Profesor.objects.get(id=taller["profesor"])
+        taller["categoria"] = Categoria.objects.get(id=taller["categoria"])
+        taller["lugar"] = Lugar.objects.get(id=taller["lugar"])
+
+    data = {
+        "talleres": talleres
+    }
+
+    return render(request, 'core/index.html', data)
 
 def loginUser(request):
     if request.user.is_authenticated:
@@ -37,3 +62,9 @@ def logoutUser(request):
     logout(request)
     messages.success(request, 'Sesion cerrada correctamente')
     return redirect('home')
+
+def verTalleres(request):
+    if request.user.is_anonymous:
+        return redirect('home')
+    
+    return render(request, 'core/talleres.html')
